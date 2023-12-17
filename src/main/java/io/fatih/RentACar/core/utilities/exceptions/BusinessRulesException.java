780cf08c06841409f8ca5015d6b62e59094569d2
Fatih Şükran <fth.21.81@gmail.com>
2023-12-17 11:30:33 +0300

    BusinessRulesException

---
+++ src/main/java/io/fatih/RentACar/core/utilities/exceptions/BusinessRulesException.java
@@ -0,0 +1,6 @@
+package io.fatih.RentACar.core.utilities.exceptions;
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public class BusinessRulesException extends RuntimeException {}
