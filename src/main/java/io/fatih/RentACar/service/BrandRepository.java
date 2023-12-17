b8a3b19fe72dd0a96951cfb371a0aa0d750f7b15
Fatih Şükran <fth.21.81@gmail.com>
2023-12-17 11:31:03 +0300

    added existsByName query to brand repository

--- src/main/java/io/fatih/RentACar/service/BrandRepository.java
+++ src/main/java/io/fatih/RentACar/service/BrandRepository.java
@@ -3,4 +3,7 @@ package io.fatih.RentACar.service;
 import io.fatih.RentACar.entities.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BrandRepository extends JpaRepository<Brand, Long> {}
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+    // check if brand name exists
+    boolean existsByName(String name);
+}
