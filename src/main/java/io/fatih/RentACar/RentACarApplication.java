c42b803df01448ea0a5803ad8db258ecc843d363
Fatih Şükran <fth.21.81@gmail.com>
2023-12-17 11:31:17 +0300

    create exception handlers

--- src/main/java/io/fatih/RentACar/RentACarApplication.java
+++ src/main/java/io/fatih/RentACar/RentACarApplication.java
@@ -1,8 +1,17 @@
 package io.fatih.RentACar;
 
+import io.fatih.RentACar.business.responses.errors.ErrorResponse;
+import io.fatih.RentACar.core.utilities.exceptions.BusinessRulesException;
+import io.fatih.RentACar.business.responses.errors.ValidationErrorResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 @SpringBootApplication
 public class RentACarApplication {
 
@@ -10,4 +19,29 @@ public class RentACarApplication {
         SpringApplication.run(RentACarApplication.class, args);
     }
 
+    @ExceptionHandler
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public ErrorResponse  handleBusinessRulesException(BusinessRulesException ex) {
+        var errorResponse = new ErrorResponse();
+        errorResponse.setMessage(ex.getMessage());
+        errorResponse.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        errorResponse.setError(HttpStatus.BAD_REQUEST);
+
+        return errorResponse;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse handleValidationException(MethodArgumentNotValidException ex) {
+        var errorResponse = new ValidationErrorResponse();
+        errorResponse.setMessage("Validation error");
+        errorResponse.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        errorResponse.setError(HttpStatus.BAD_REQUEST);
+
+        for (var error : ex.getBindingResult().getFieldErrors()) {
+            errorResponse.getValidationErrors().put(error.getField(), error.getDefaultMessage());
+        }
+
+        return errorResponse;
+    }
 }
