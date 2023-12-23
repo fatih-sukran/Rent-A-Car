c07c45aafdb8157067cf9f42fbe7eae136eda7f5
Fatih Şükran <fth.21.81@gmail.com>
2023-12-23 19:24:26 +0300

    implements mappers to service

--- src/main/java/io/fatih/RentACar/service/CarRepository.java
+++ src/main/java/io/fatih/RentACar/service/CarRepository.java
@@ -3,4 +3,6 @@ package io.fatih.RentACar.service;
 import io.fatih.RentACar.entities.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CarRepository extends JpaRepository<Car, Long> {}
+public interface CarRepository extends JpaRepository<Car, Long> {
+    boolean existsByPlate(String plate);
+}
--- src/main/java/io/fatih/RentACar/service/ModelRepository.java
+++ src/main/java/io/fatih/RentACar/service/ModelRepository.java
@@ -3,4 +3,6 @@ package io.fatih.RentACar.service;
 import io.fatih.RentACar.entities.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ModelRepository extends JpaRepository<Model, Long> {}
+public interface ModelRepository extends JpaRepository<Model, Long> {
+    boolean existsByName(String name);
+}
